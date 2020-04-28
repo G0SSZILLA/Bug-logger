4f2695d5ed6d72d1a89203010ff44967dcf1a633
G0SSZILLA <gossman15@gmail.com>
2020-04-28 08:49:09 -0600

    layout changed and design touch ups

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,124 +1,132 @@
 
 <template>
-  <div class="home container-fluid ">
-<div class="row">
-  <div class="col-5 text-center text-white mt-5 pl-5">
-    <h1>Current Bugs</h1>
-  </div>
-  <div class="col-4"></div>
-  <div class="col-3 text-white pt-5">
-    <!-- Material unchecked -->
-<div class="form-check">
-    <input type="checkbox" @click="toggleClosed()" class="form-check-input" id="materialUnchecked">
-    <label class="form-check-label" for="materialUnchecked">Filter Closed</label>
-</div>
-  </div>
-</div>
-<div class="row">
-  <div class="col-8 m-auto">
-<div class="row mt-5 text-center text-white ">
-      <div class="col-2">Title</div>
-      <div class="col-3">Description</div>
-      <div class="col-3">Reported By</div>
-      <div class="col-2">Status</div>
-      <div class="col-2">Last Modified</div>
-</div>
-<hr class="border-white">
-  </div>
-</div>
-<div class="row">
-  <div class="col-8 m-auto" >
-   
-     <Bug v-for="bug in bugs" :openBugs='showOpenBugs' :bugData='bug' :key="bug.id" />
-  </div>
-</div>
-
-
-<div class="modal fade" id="orangeModalSubscription" tabindex="-1"  aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog modal-notify modal-warning" role="document">
-    <!--Content-->
-    <div class="modal-content">
-      <!--Header-->
-      <div class="modal-header text-center">
-        <h4 class="modal-title white-text w-100 font-weight-bold py-2">Log a Bug</h4>
-        <button type="button" class="close" data-dismiss="modal " aria-label="Close">
-          <span aria-hidden="true" class="white-text">&times;</span>
-        </button>
+  <div class="home container-fluid">
+    <div class="row">
+      <div class="col-5 text-center text-white mt-5 pl-5 ml-5">
+        <h1>Current Bugs</h1>
       </div>
+      <div class="col-3 mt-5 pt-3">
+        <div
+          class="modal fade"
+          id="orangeModalSubscription"
+          tabindex="-1"
+          aria-labelledby="myModalLabel"
+          aria-hidden="true">
+          <div class="modal-dialog modal-notify modal-warning" role="document">
+            <!--Content-->
+            <div class="modal-content">
+              <!--Header-->
+              <div class="modal-header text-center">
+                <h4 class="modal-title white-text w-100 font-weight-bold py-2">Log a Bug</h4>
+                <button type="button" class="close" data-dismiss="modal " aria-label="Close">
+                  <span aria-hidden="true" class="white-text">&times;</span>
+                </button>
+              </div>
 
-      <!--Body-->
-      <div class="modal-body">
-        <div class="md-form mb-5">
-          <i class="fas fa-user prefix grey-text"></i>
-          <input type="text" id="form3" class="form-control "  v-model="newBug.title">
-          <label data-error="wrong" data-success="right" for="form3">Title</label>
+              <!--Body-->
+              <div class="modal-body">
+                <div class="md-form mb-5">
+                  <i class="fas fa-user prefix grey-text"></i>
+                  <input type="text" id="form3" class="form-control" v-model="newBug.title" />
+                  <label data-error="wrong" data-success="right" for="form3">Title</label>
+                </div>
+
+                <div class="md-form">
+                  <i class="fas fa-envelope prefix grey-text"></i>
+                  <input type="email" id="form2" class="form-control" v-model="newBug.description" />
+                  <label data-error="wrong" data-success="right" for="form2">Description</label>
+                </div>
+              </div>
+
+              <!--Footer-->
+              <div class="modal-footer justify-content-center">
+                <button
+                  type="button"
+                  class="btn btn-primary waves-effect ml-5"
+                  data-dismiss="modal"
+                  @click.prevent="addBug()"
+                >Add Bug</button>
+              </div>
+            </div>
+            <!--/.Content-->
+          </div>
         </div>
-      
-<div class="md-form">
-          <i class="fas fa-envelope prefix grey-text"></i>
-          <input type="email" id="form2" class="form-control "  v-model="newBug.description">
-          <label data-error="wrong" data-success="right" for="form2">Description</label>
+
+        <div class="text-center">
+          <a
+            href
+            class="btn bg-warning btn-rounded"
+            data-toggle="modal"
+            data-target="#orangeModalSubscription"
+          >Log a Bug!</a>
         </div>
-      
       </div>
-
-      <!--Footer-->
-      <div class="modal-footer justify-content-center">
-        <button type="button" class="btn btn-primary waves-effect" data-dismiss="modal" @click.prevent="addBug()">Add</button>
+      <div class="col-3 text-white mt-5 pt-4">
+        <div class="form-check">
+          <input
+            type="checkbox"
+            @click="toggleClosed()"
+            class="form-check-input"
+            id="materialUnchecked"/>
+          <label class="form-check-label" for="materialUnchecked">Filter Closed</label>
+        </div>
       </div>
     </div>
-    <!--/.Content-->
-  </div>
-</div>
-
-<div class="text-center">
-  <a href="" class="btn bg-white btn-rounded" data-toggle="modal" data-target="#orangeModalSubscription">Log a Bug!</a>
-</div>
-
-
-
-  
+    <div class="row">
+      <div class="col-8 m-auto">
+        <div class="row mt-5 text-center text-white">
+          <div class="col-2">Title</div>
+          <div class="col-3">Description</div>
+          <div class="col-3">Reported By</div>
+          <div class="col-2">Status</div>
+          <div class="col-2">Last Modified</div>
+        </div>
+        <hr class="border-white" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-8 m-auto">
+        <Bug v-for="bug in bugs" :openBugs="showOpenBugs" :bugData="bug" :key="bug.id" />
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
-import Bug from '../components/Bug'
+import Bug from "../components/Bug";
 export default {
-  name: 'home',
-  props:['bugData'],
-  data(){
+  name: "home",
+  props: ["bugData"],
+  data() {
     return {
       newBug: {},
       showOpenBugs: false
-      };
+    };
   },
-  mounted(){
+  mounted() {
     this.$store.dispatch("getBugs", this.$route.params.bugId);
     this.$store.dispatch("getNotes", this.$route.params.bugId);
   },
-  computed:{
-      bugs() {
+  computed: {
+    bugs() {
       return this.$store.state.bugs;
     }
   },
-  methods:{
+  methods: {
     addBug() {
       this.$store.dispatch("addBug", this.newBug);
-        this.newBug = {};
-    
+      this.newBug = {};
     },
 
-    toggleClosed(){
+    toggleClosed() {
       console.log(this.showOpenBugs);
-      
-this.showOpenBugs = !this.showOpenBugs
+
+      this.showOpenBugs = !this.showOpenBugs;
     }
-     
   },
-  components:{Bug}
-}
+  components: { Bug }
+};
 </script>
 
 
--- clientsrc/src/components/Bug.vue
+++ clientsrc/src/components/Bug.vue
@@ -1,46 +1,45 @@
 <template>
   <div class="Bug text-center">
-    
-     <div class="row border-bottom border-success  text-white mt-0" @click="selectBug()" v-if="openBugs && !bugData.closed || !openBugs">
-
+    <div
+      class="row border-bottom border-info text-white mt-0"
+      @click="selectBug()"
+      v-if="openBugs && !bugData.closed || !openBugs">
       <div class="col-2">
-      <h4>{{bugData.title}}</h4>
+        <h4>{{bugData.title}}</h4>
       </div>
       <div class="col-3 text-center">
-      <h4>{{bugData.description}}</h4>
+        <h4>{{bugData.description}}</h4>
       </div>
       <div class="col-3">
-      <h4>{{bugData.creatorEmail}}</h4>
+        <h4>{{bugData.creatorEmail}}</h4>
       </div>
       <span class="col-2">
-      <div v-if="bugData.closed == true" class=" pl-3 text-danger text-center">
-      <h4>Closed</h4>
-      </div>
-        <div v-else class="text-success"> <h4>Open</h4></div>
+        <div v-if="bugData.closed == true" class="pl-3 text-danger text-center">
+          <h4>Closed</h4>
+        </div>
+        <div v-else class="text-success">
+          <h4>Open</h4>
+        </div>
       </span>
-         <div class="col-2">
-      <h4>{{date()}}</h4>
+      <div class="col-2">
+        <h4>{{date()}}</h4>
       </div>
-     </div>
-      
-  <hr class="border-white">
+    </div>
+    <hr />
   </div>
-      
-    
-  
 </template>
 
 
 <script>
 export default {
   name: "Bug",
-  props: ["bugData", 'openBugs'],
+  props: ["bugData", "openBugs"],
   data() {
     return {};
   },
   computed: {},
   methods: {
-   selectBug() {
+    selectBug() {
       this.$store.commit("setActiveBug", {});
       this.$router.push({
         name: "Details",
@@ -48,9 +47,9 @@ export default {
       });
     },
     date() {
-      let date = this.bugData.updatedAt.split('T')
-      return date[0]
-    },
+      let date = this.bugData.updatedAt.split("T");
+      return date[0];
+    }
   },
   components: {}
 };
